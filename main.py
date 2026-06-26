2f30695f26951e03ef86fa6a332db4e59f03d701
subodh Kute <subodhkute07@gmail.com>
2026-06-26 18:55:58 +0530

    Add GET /Patient/{id} endpoint

--- main.py
+++ main.py
@@ -21,3 +21,13 @@ def create_patient(patient: Patient):
         "name": patient.name,
         "dateOfBirth": patient.dob
     }
+
+@app.get("/patient/{patient_id}")
+def get_patient(patient_id: str):
+    #For now, return mock data (we'll add database later)
+    return { 
+        "resourceType"  : "Patient",
+        "id" : patient_id,
+        "name"  :  "John Doe",
+        "dateOfBirth" : "1990-01-01"
+    }
